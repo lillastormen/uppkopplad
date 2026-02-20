b905651135094c50a2ed54ed88c42be7bc57f8f2
Karolina <karolina.limanowska@gmail.com>
2026-02-20 17:06:43 +0100

    cleaning in server.ts and app.ts files

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,7 @@ import userRoutes from "./routes/userRoutes.ts";
 import quizRoutes from "./routes/quizRoutes.ts";
 import lessonRoutes from "./routes/moduleRoutes.ts";
 import "dotenv/config";
-import { startMongoServer } from "./server.ts";
-import cors from "cors";
 import "dotenv/config";
-// import { startMongoServer } from './server.js';
 import router from "./routes/moduleRoutes.ts";
 
 const app = express();
@@ -17,12 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-(async () => {
-  await startMongoServer();
-  app.listen(port, () => {
-    console.log(`App is listening on port: ${port}`);
-  });
-})();
+
 //Users Routes
 app.use("/users", userRoutes);
 
@@ -32,11 +24,5 @@ app.use("/api/lessons", router);
 //Quiz Routes
 app.use("/quiz", quizRoutes);
 
-// (async () => {
-//     await startMongoServer();
-//     app.listen(port, () => {
-//         console.log(`App is listening on port: ${port}`);
-//     });
-// })();
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,7 @@
 import "dotenv/config";
 import { mongoDBConnect } from "./db/mongo.ts";
 import app from "./app.ts";
-import mySqlDbConnection from "./db/mysql.ts";
 import { connectMySQL } from "./db/mysql.ts";
-
-export async function startMongoServer() {
-  try {
-    await mongoDBConnect();
-    console.log("Connected to mongoDB");
-  } catch (err) {
-    console.error("Server could not start: ", err);
-    process.exit(1);
-  }
-}
 
 const PORT = Number(process.env.PORT) || 3000;
 
@@ -30,39 +19,5 @@ async function startServer() {
     process.exit(1);
   }
 }
-// export async function startMongoServer() {
-//     try {
-//         await mongoDBConnect();
-//         console.log('Connected to mongoDB');
-//     } catch (err) {
-//         console.error('Server could not start: ', err);
-//         process.exit(1);
-//     }
-// }
-
-// mySqlDbConnection.connect((error) => {
-//     if (error) {
-//         console.log('MySQL connection failed: ', error);
-//         process.exit(1);
-//     }
-
-mySqlDbConnection.connect(error => {
-  if (error) {
-    console.log("MySQL connection failed: ", error);
-    process.exit(1);
-  }
-
-  console.log("MySQL connected successfully");
-
-  app.listen(PORT, () => {
-    console.log(`Server started and listening on port: ${PORT}`);
-  });
-});
-//     console.log('MySQL connected successfully');
-
-//     app.listen(PORT, () => {
-//         console.log(`Server started and listening on port: ${PORT}`);
-//     });
-// });
 
 startServer();
