3482de1888196a90ffdc2959183fe017525539e0
Karolina <karolina.limanowska@gmail.com>
2026-02-25 18:43:22 +0100

    smth is bugging

---
+++ src/config/session.ts
@@ -0,0 +1,36 @@
+import dotenv from 'dotenv';
+import session from 'express-session';
+import MySQLSession from 'express-mysql-session';
+import { reqEnv } from '../db/mysql.ts';
+
+
+dotenv.config();
+
+
+export function createSession() {
+
+    const mySQLStore = MySQLSession(session);
+
+    const store = new mySQLStore({
+        host: reqEnv('DB_HOST'),
+        port: Number(process.env.DB_PORT),
+        user: reqEnv('DB_USER'),
+        password: reqEnv('DB_PASSWORD'),
+        database: reqEnv('DB_DATABASE')
+    });
+    
+    return session({
+        name: 'sid',
+        secret: process.env.SESSION_SECRET! as string,
+        resave: false,
+        saveUninitialized: false,
+        store,
+        rolling: true, //refresh cookie expiration on each request - stays loggrf in while
+        cookie: {
+            httpOnly: true,
+            sameSite: "lax",
+            secure: false, // true in production
+            maxAge: 1000 * 60 * 60 * 24 * 7,
+        },
+    });
+}
