3d4d7e44e4ecd0ee497db1bb8e6cad5efbf9ee67
Mathias Granlöf <granlofmathias@gmail.com>
2026-02-25 15:49:19 +0100

    quiz working

--- public/quiz/quiz.js
+++ public/quiz/quiz.js
@@ -4,15 +4,75 @@ const form = document.querySelector("#quizForm");
 const next = document.querySelector("#quizNextBtn");
 const radio = document.querySelector("#radios");
 const checkbox = document.querySelector("#checkboxes");
+const noSelected = document.querySelector("#noSelected");
+const quizSection = document.querySelector("#quiz");
 let currentQuestion = 0;
 let currentQuiz;
+let userAnswers = [];
+let correctAnswers = [];
+let result = [];
+let type = "";
+let label;
 
 form.addEventListener("submit", e => {
   e.preventDefault();
+  form.style.display = "none";
+  console.log(userAnswers);
+  console.log(correctAnswers);
+
+  for (let i = 0; i < correctAnswers.length; i++) {
+    if (String(correctAnswers[i]) == String(userAnswers[i])) {
+      result.push(true);
+    } else {
+      result.push(false);
+    }
+  }
+  const trueAmount = result.filter(t => t === true).length;
+  const resultText = document.createElement("p");
+  resultText.textContent = `Ditt fick ${trueAmount} rätt av totalt ${result.length}`;
+  quizSection.appendChild(resultText);
 });
+
 next.addEventListener("click", () => {
-  currentQuestion++;
-  showQuestion(currentQuiz);
+  correctAnswers = currentQuiz.map(q =>
+    q.answers.filter(a => a.is_correct).map(a => a.answer),
+  );
+  let userSelected;
+
+  //RADIO
+  if (type === "radio") {
+    userSelected = document.querySelector('input[name="question"]:checked');
+    if (!userSelected) {
+      noSelected.style.display = "inline";
+      return;
+    }
+
+    userAnswers[currentQuestion] = [userSelected.value];
+  }
+  //CHECKBOX
+  else if (type === "checkbox") {
+    userSelected = [
+      ...document.querySelectorAll('input[name="question"]:checked'),
+    ].map(i => i.value);
+    if (userSelected.length === 0) {
+      noSelected.style.display = "inline";
+      return;
+    }
+    userAnswers[currentQuestion] = userSelected;
+  }
+
+  noSelected.style.display = "none";
+
+  //PROCEED
+  if (userSelected) {
+    currentQuestion++;
+    if (currentQuiz[currentQuestion]) {
+      showQuestion(currentQuiz);
+    } else {
+      next.style.display = "none";
+      submit.style.display = "block";
+    }
+  }
 });
 
 async function loadQuiz(id) {
@@ -28,41 +88,31 @@ async function loadQuiz(id) {
 }
 
 function showQuestion(quiz) {
-  console.log(quiz);
-  if (quiz.length == currentQuestion) {
-    next.style.display = "none";
-    submit.style.display = "block";
-  }
-
   question.textContent = quiz[currentQuestion].question;
-
   if (quiz[currentQuestion].multiple_choices) {
+    type = "checkbox";
     checkbox.style.display = "flex";
     radio.style.display = "none";
-
     checkbox.replaceChildren();
-
-    quiz[currentQuestion].answers.forEach(answer => {
-      const label = document.createElement("label");
-      label.innerHTML = `
-      <input type="checkbox" name=question value="${answer.answer}">${answer.answer}
-      `;
-      checkbox.appendChild(label);
-    });
   } else {
+    type = "radio";
     checkbox.style.display = "none";
     radio.style.display = "flex";
     radio.replaceChildren();
-
-    quiz[currentQuestion].answers.forEach(answer => {
-      const label = document.createElement("label");
-      label.innerHTML = `
-      <input type="radio" name=question value="${answer.answer}">${answer.answer}
-      `;
-
-      radio.appendChild(label);
-    });
   }
+  quiz[currentQuestion].answers.forEach(a => {
+    label = document.createElement("label");
+    label.innerHTML = `
+      <input type=${type} name=question value="${a.answer}">${a.answer}
+      `;
+    if (type === "radio") {
+      radio.appendChild(label);
+    } else if (type === "checkbox") {
+      checkbox.appendChild(label);
+    }
+  });
+  const inputs = document.querySelectorAll('input[name="question"]');
+  inputs.forEach(i => (i.checked = false));
 }
 
 const quiz = {
