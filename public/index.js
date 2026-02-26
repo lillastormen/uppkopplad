2bb2891535ee6de4512bd3bda1d8f38f30c91a15
Karolina <karolina.limanowska@gmail.com>
2026-02-26 23:53:00 +0100

    adding includeHTML function, working a little bit on styling

--- public/index.js
+++ public/index.js
@@ -1,3 +1,19 @@
 import quiz from "./quiz/quiz.js";
 
 quiz.loadQuiz(2);
+
+function includeHTML() {
+
+    const includes = document.querySelectorAll('[data-include]');
+
+    includes.forEach(element => {
+        const file = element.getAttribute('data-include');
+
+        fetch(file)
+            .then(response => response.text())
+            .then(html => {element.innerHTML = html;
+        });
+    });
+}
+
+document.addEventListener("DOMContentLoaded", includeHTML);
