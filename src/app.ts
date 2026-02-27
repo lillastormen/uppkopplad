500eba0313fe2426da09d9b25f1aa20ea446eb1b
Patrik Drevestedt <patrik.drevestedt@me.com>
2026-02-27 19:18:46 +0100

    fix cors issues

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,35 @@
-import express from "express";
+import express, { type Request, type Response, type NextFunction } from "express";
 import userRoutes from "./routes/userRoutes.ts";
 import quizRoutes from "./routes/quizRoutes.ts";
+import modulesRoutes from './routes/moduleRoutes.ts';
 import "dotenv/config";
 import cors from "cors";
-import modulesRoutes from "./routes/moduleRoutes.ts";
 import { createSession } from "./config/session.ts";
-import routes from "./routes/moduleRoutes.ts";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(createSession());
 app.use(express.static("public"));
-app.use(cors());
 
 //Users Routes
 app.use("/users", userRoutes);
 
 // Main modules Routes
-app.use("/api/mainModules", routes.mainModulesRoutes);
+app.use("/api/mainModules", modulesRoutes.mainModulesRoutes);
 
 //Lessons Routes
-app.use("/api/lessons", routes.lessonsRoutes);
+app.use("/api/lessons", modulesRoutes.lessonsRoutes);
 
 //Quiz Routes
 app.use("/quiz", quizRoutes);
+
+// For global error handling
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+        console.error('Global error handler: ', err.message, err.stack);
+        res.status(500).json({ error: 'Internal server error' });
+});
 
 export default app;
