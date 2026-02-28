39a8dafae540c3ca45bb0bda4e838fc7795f440e
Karolina <karolina.limanowska@gmail.com>
2026-03-01 00:42:30 +0100

    fixing absolute paths to handle includeHTML function correctly

--- public/index.js
+++ public/index.js
@@ -1,19 +1,5 @@
 import quiz from "./quiz/quiz.js";
 
-export function includeHTML() {
 
-    const includes = document.querySelectorAll('[data-include]');
-
-    includes.forEach(element => {
-        const file = element.getAttribute('data-include');
-
-        fetch(file)
-            .then(response => response.text())
-            .then(html => {element.innerHTML = html;
-        });
-    });
-}
-
-document.addEventListener("DOMContentLoaded", includeHTML);
 
 quiz.loadQuiz(1);
--- public/registration/registration.js
+++ public/registration/registration.js
@@ -1,6 +1,12 @@
+import { includeHTML } from '../includeHTML.js'
+
+includeHTML();
+
 const url = 'http://localhost:3000/users/registration';
 const form = document.getElementById('registration-form');
 const message = document.getElementById('registration-message');
+
+
 
 form.addEventListener('submit', async (event) => {
     event.preventDefault();
